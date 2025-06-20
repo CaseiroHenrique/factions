1b5361c52b55fa29ee79429b020aa51c046058c3
Conexão Code <caseiro.henrique@gmail.com>
2025-06-20 16:05:46 -0300

    Update factions tablist to support multiple lines

--- core/factions/src/main/java/conexao/code/manager/TabListManager.java
+++ core/factions/src/main/java/conexao/code/manager/TabListManager.java
@@ -5,6 +5,9 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class TabListManager {
     private final Plugin plugin;
 
@@ -13,10 +16,14 @@ public class TabListManager {
     }
 
     public void applyAll() {
-        String header = ChatColor.translateAlternateColorCodes('&',
-                plugin.getConfig().getString("tablist.header", ""));
-        String footer = ChatColor.translateAlternateColorCodes('&',
-                plugin.getConfig().getString("tablist.footer", ""));
+        List<String> headerLines = plugin.getConfig().getStringList("tablist.headerLines");
+        List<String> footerLines = plugin.getConfig().getStringList("tablist.footerLines");
+        String header = headerLines.stream()
+                .map(line -> ChatColor.translateAlternateColorCodes('&', line))
+                .collect(Collectors.joining("\n"));
+        String footer = footerLines.stream()
+                .map(line -> ChatColor.translateAlternateColorCodes('&', line))
+                .collect(Collectors.joining("\n"));
         for (Player player : Bukkit.getOnlinePlayers()) {
             player.setPlayerListHeaderFooter(header, footer);
         }
